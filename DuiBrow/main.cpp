97d2a9dfc432ecc1f846c732e25ab61a09e46f3e
yilanshuai <yilanshuai@win-trust.cn>
2021-02-23 17:27:18 +0800

    添加标签页关闭功能

--- DuiBrow/main.cpp
+++ DuiBrow/main.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 #include <tchar.h>
 #include "stdafx.h"
+#include <sstream>
+#include <stdio.h>
+#include <list> 
 
 #pragma once
 #include "../DuiLib/UIlib.h"
@@ -31,10 +34,9 @@ public:
 		CHorizontalLayoutUI* tabstrip1 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip1")));
 		m_CurrentTabStrip = tabstrip1;
 	}
+
 	virtual void Notify(TNotifyUI& msg)
 	{
-		
-
 		CDuiString sCtrlName = msg.pSender->GetName();
 		if (msg.sType == _T("click"))
 		{
@@ -62,101 +64,123 @@ public:
 				SendMessage(WM_SYSCOMMAND, SC_RESTORE, 0);
 				return;
 			}
-			else if (sCtrlName.Find(_T("tab-option-"), 0) != -1)
-			{
-				//增加页签后默认选择焦点在新建的页签
-				std::wstring ctrlName(sCtrlName.GetData());
-				//SelectTab(ctrlName.substr(11));
-			}
+			//else if (sCtrlName.Find(_T("tab-option-"), 0) != -1)
+			//{
+			//	//增加页签后默认选择焦点在新建的页签
+			//	std::wstring ctrlName(sCtrlName.GetData());
+			//	SelectTab(ctrlName.substr(11));
+			//}
 			else if (sCtrlName.Find(_T("tab-close-"), 0) != -1)
 			{
 				//关闭页签
 				std::wstring closeName(sCtrlName.GetData());
-				//CloseTab(closeName.substr(10));
+				CloseTab(closeName.substr(10));
 			}
-			else if (sCtrlName == _T("explore"))
+			else if (sCtrlName == _T("homepageBtn"))
 			{	
-				CVerticalLayoutUI* sidevlayout = static_cast<CVerticalLayoutUI*>(m_PaintManager.FindControl(_T("side_vlayout")));
-				CHorizontalLayoutUI* tabstrip1 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip1")));
-				UINT tabindex = tabstrip1->GetCount();
-				UINT sideindex = sidevlayout->GetCount();
-
-				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
-				CTabLayoutUI* m_ContentView1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView1")));
+				//查找当前页所在ContentTab和Tabstrip
 				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
 				CTabLayoutUI* m_ContentTab1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab1")));
-				m_ContentView->SelectItem(m_ContentView1);
 				m_ContentTab->SelectItem(m_ContentTab1);
+
+				CHorizontalLayoutUI* tabstrip1 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip1")));
 				m_CurrentTabStrip = tabstrip1;
-			
 				
+				//查找当前页所在ContentView
+				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
+				CTabLayoutUI* m_ContentView1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView1")));
+				m_ContentView->SelectItem(m_ContentView1);	
 			}
-			else if (sCtrlName == _T("personalinfo"))
+			else if (sCtrlName == _T("personinfoBtn"))
 			{
-				CVerticalLayoutUI* sidevlayout = static_cast<CVerticalLayoutUI*>(m_PaintManager.FindControl(_T("side_vlayout")));
+				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
+				CTabLayoutUI* m_ContentTab2 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab2")));
+				m_ContentTab->SelectItem(m_ContentTab2);
+
 				CHorizontalLayoutUI* tabstrip2 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip2")));
-				UINT tabindex = tabstrip2->GetCount();
-				UINT sideindex = sidevlayout->GetCount();
+				m_CurrentTabStrip = tabstrip2;
 
 				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
 				CTabLayoutUI* m_ContentView2 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView2")));
 				m_ContentView->SelectItem(m_ContentView2);
-				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
-				CTabLayoutUI* m_ContentTab2 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab2")));
-				m_ContentTab->SelectItem(m_ContentTab2);
-				m_CurrentTabStrip = tabstrip2;
-
-
 				
-
 			}
-			else if (sCtrlName == _T("modifypassword"))
+			else if (sCtrlName == _T("modifypasswordBtn"))
 			{
-				CVerticalLayoutUI* sidevlayout = static_cast<CVerticalLayoutUI*>(m_PaintManager.FindControl(_T("side_vlayout")));
+				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
+				CTabLayoutUI* m_ContentTab3 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab3")));
+				m_ContentTab->SelectItem(m_ContentTab3);
+
 				CHorizontalLayoutUI* tabstrip3 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip3")));
-				UINT tabindex = tabstrip3->GetCount();
-				UINT sideindex = sidevlayout->GetCount();
+				m_CurrentTabStrip = tabstrip3;
 
 				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
 				CTabLayoutUI* m_ContentView3 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView3")));
 				m_ContentView->SelectItem(m_ContentView3);
-				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
-				CTabLayoutUI* m_ContentTab3 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab3")));
-				m_ContentTab->SelectItem(m_ContentTab3);
-				m_CurrentTabStrip = tabstrip3;
-
-				
 			}
-			else if (sCtrlName == _T("Add1"))
+			else if (sCtrlName == _T("NewTabButton"))
 			{
 				UINT tabindex = m_CurrentTabStrip->GetCount();
-
-
 				CContainerUI* pTab = new CContainerUI;
-				/*std::wostringstream oss;
-				oss << L"maxwidth=\"174\" name=\"tab-" << id << L"\"";
-				pTab->SetAttributeList(oss.str().c_str());*/
 				CDialogBuilder builder;
 				builder.Create(_T("tab.xml"), (UINT)0, NULL, &m_PaintManager, pTab);
 				m_CurrentTabStrip->AddAt(pTab, tabindex);
 
 				COptionUI* option = static_cast<COptionUI*>(m_PaintManager.FindSubControlByClass(pTab, _T("Option"), 0));
-				//option->SetName((std::wstring(L"tab-option-") + id).c_str());
 				option->SetGroup(L"group-browser");
 				option->SetText(L"新标签页");
 				option->SetTextPadding({ 12, 0, 26, 0 });
 				option->SetFont(0);
-				//CButtonUI* close = static_cast<CButtonUI*>(m_PaintManager.FindSubControlByClass(pTab, _T("Button"), 0));
-				//std::wostringstream closeAttributes;
-				//closeAttributes << L"name=\"tab-close-" << id << L"\"";
-				//close->SetAttributeList(closeAttributes.str().c_str());
-				/*BrowserBundle* newBundle = new BrowserBundle();
-				newBundle->closeButton = close;
-				newBundle->tabButton = option;
-				newBundle->tab = pTab;*/
+
+				CButtonUI* pbutton = static_cast<CButtonUI*>(m_PaintManager.FindSubControlByClass(pTab, _T("Button"), 0));
+
+				std::wostringstream closeAttributes;
+			
+				std::wstring Tid = GetUniqueid();
+				closeAttributes << L"name=\"tab-close-" << Tid << L"\"";
+				pbutton->SetAttributeList(closeAttributes.str().c_str());
+
+				BrowserBundle* bundle = new BrowserBundle(Tid);
+				bundle->tab = pTab;
+				bundleList.push_back(bundle);
 			}
 		}
 	}
+
+std::wstring GetUniqueid()
+{
+	wchar_t buf[128] = { 0 };
+	GUID guid;
+	CoInitialize(NULL);
+	if (S_OK == ::CoCreateGuid(&guid))
+	{
+		swprintf(buf, sizeof(buf),
+			L"%08X%04X%04X%02X%02X%02X%02X%02X%02X%02X%02X",
+			guid.Data1,
+			guid.Data2,
+			guid.Data3,
+			guid.Data4[0], guid.Data4[1],
+			guid.Data4[2], guid.Data4[3],
+			guid.Data4[4], guid.Data4[5],
+			guid.Data4[6], guid.Data4[7]);
+	}
+	CoUninitialize();
+	return buf;
+}
+
+void CloseTab(std::wstring id)
+{
+	list<BrowserBundle*>::iterator it;
+	for (it = bundleList.begin(); it != bundleList.end(); it++) 
+	{
+		BrowserBundle* bundle = *it;
+		if (bundle->id == id)
+		{
+			static_cast<CContainerUI*>(bundle->tab->GetParent())->Remove(bundle->tab, true);
+			break;
+		}
+	}
+}
 
 virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -177,8 +201,19 @@ virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return __super::HandleMessage(uMsg, wParam, lParam);
 
 	}
+
+struct BrowserBundle
+{
+	std::wstring id;
+	CContainerUI* tab;
+	BrowserBundle(std::wstring id) : id(id) {}
+};
+
+std::list <BrowserBundle*> bundleList;
+
 private:
 	CHorizontalLayoutUI* m_CurrentTabStrip;
+		
 };
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
@@ -187,7 +222,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
 
 	CDuiFrameWnd duiFrame;
-	duiFrame.Create(NULL, _T("DuiBrow"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.Create(NULL, _T("DuilibDemo"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
 	duiFrame.CenterWindow();
 	duiFrame.ShowModal();
 	return 0;
