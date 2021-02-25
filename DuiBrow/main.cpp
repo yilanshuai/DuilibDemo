5103511000a5ae27ad0e7d82845d040749c7b95a
yilanshuai <yilanshuai@win-trust.cn>
2021-02-25 09:13:24 +0800

    [add]添加标签页标题更新、标签页切换

--- DuiBrow/main.cpp
+++ DuiBrow/main.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <windows.h>
 #include <tchar.h>
 #include "stdafx.h"
@@ -5,25 +6,22 @@
 #include <stdio.h>
 #include <list> 
 
-#pragma once
-#include "UIlib.h"
+#include <XBrowser/UICEFBrowser.h>
+#include <XBrowser/UIMIEBrowser.h>
+
+#include "duilib/UIlib.h"
+#include "include/cef_app.h"
+
+using namespace XBrowser;
 using namespace DuiLib;
 
-#ifdef _DEBUG
-#   ifdef _UNICODE
-#       pragma comment(lib, "DuiLib.lib")
-#   else
-#       pragma comment(lib, "DuiLib.lib")
-#   endif
-#else
-#   ifdef _UNICODE
-#       pragma comment(lib, "DuiLib.lib")
-#   else
-#       pragma comment(lib, "DuiLib.lib")
-#   endif
-#endif
+#pragma comment(lib, "cef_sandbox.lib")
+#pragma comment(lib, "libcef.lib")
+#pragma comment(lib, "libcef_dll_wrapper.lib")
+#pragma comment(lib, "DuiLib.lib")
+#pragma comment(lib, "XCBrowser.lib")
 
-class CDuiFrameWnd : public WindowImplBase
+class CDuiFrameWnd : public WindowImplBase, public XBrowser::BrowserDelegate
 {
 public:
 	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
@@ -31,8 +29,13 @@ public:
 	virtual CDuiString GetSkinFolder() { return _T(""); }
 	virtual void InitWindow()
 	{
+		m_CEFBrowser = new XBrowser::CCEFBrowserUI(GetHWND(), this);
+
 		CHorizontalLayoutUI* tabstrip1 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip1")));
 		m_CurrentTabStrip = tabstrip1;
+
+		CTabLayoutUI* m_ContentView1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView1")));
+		m_ContentView1->Add(m_CEFBrowser);
 	}
 
 	virtual void Notify(TNotifyUI& msg)
@@ -64,12 +67,6 @@ public:
 				SendMessage(WM_SYSCOMMAND, SC_RESTORE, 0);
 				return;
 			}
-			//else if (sCtrlName.Find(_T("tab-option-"), 0) != -1)
-			//{
-			//	//增加页签后默认选择焦点在新建的页签
-			//	std::wstring ctrlName(sCtrlName.GetData());
-			//	SelectTab(ctrlName.substr(11));
-			//}
 			else if (sCtrlName.Find(_T("tab-close-"), 0) != -1)
 			{
 				//关闭页签
@@ -77,7 +74,7 @@ public:
 				CloseTab(closeName.substr(10));
 			}
 			else if (sCtrlName == _T("homepageBtn"))
-			{	
+			{
 				//查找当前页所在ContentTab和Tabstrip
 				CTabLayoutUI* m_ContentTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab")));
 				CTabLayoutUI* m_ContentTab1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentTab1")));
@@ -85,11 +82,11 @@ public:
 
 				CHorizontalLayoutUI* tabstrip1 = static_cast<CHorizontalLayoutUI*>(m_PaintManager.FindControl(_T("Tabstrip1")));
 				m_CurrentTabStrip = tabstrip1;
-				
+
 				//查找当前页所在ContentView
 				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
 				CTabLayoutUI* m_ContentView1 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView1")));
-				m_ContentView->SelectItem(m_ContentView1);	
+				m_ContentView->SelectItem(m_ContentView1);
 			}
 			else if (sCtrlName == _T("personinfoBtn"))
 			{
@@ -103,7 +100,7 @@ public:
 				CTabLayoutUI* m_ContentView = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("Content")));
 				CTabLayoutUI* m_ContentView2 = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("ContentView2")));
 				m_ContentView->SelectItem(m_ContentView2);
-				
+
 			}
 			else if (sCtrlName == _T("modifypasswordBtn"))
 			{
@@ -135,95 +132,184 @@ public:
 				CButtonUI* pbutton = static_cast<CButtonUI*>(m_PaintManager.FindSubControlByClass(pTab, _T("Button"), 0));
 
 				std::wostringstream closeAttributes;
-			
+
 				std::wstring Tid = GetUniqueid();
 				closeAttributes << L"name=\"tab-close-" << Tid << L"\"";
 				pbutton->SetAttributeList(closeAttributes.str().c_str());
 
 				BrowserBundle* bundle = new BrowserBundle(Tid);
 				bundle->tab = pTab;
+				bundle->browserId = m_CEFBrowser->NewPage(L"http://www.baidu.com");
+				bundle->TabButton = option;
 				bundleList.push_back(bundle);
+				m_CEFBrowser->ShowPage(bundle->browserId);
 			}
 		}
 	}
 
-std::wstring GetUniqueid()
-{
-	wchar_t buf[128] = { 0 };
-	GUID guid;
-	CoInitialize(NULL);
-	if (S_OK == ::CoCreateGuid(&guid))
-	{
-		swprintf(buf, sizeof(buf),
-			L"%08X%04X%04X%02X%02X%02X%02X%02X%02X%02X%02X",
-			guid.Data1,
-			guid.Data2,
-			guid.Data3,
-			guid.Data4[0], guid.Data4[1],
-			guid.Data4[2], guid.Data4[3],
-			guid.Data4[4], guid.Data4[5],
-			guid.Data4[6], guid.Data4[7]);
+	/*****************CEF相关事件********************************************************************/
+	void OnBrowserCreated(CefRefPtr<CefBrowser> browser) {}
+	void OnBrowserClosing(CefRefPtr<CefBrowser> browser) {}
+	void OnBrowserClosed(CefRefPtr<CefBrowser> browser) {}
+	void OnBrowserExit(CefRefPtr<CefBrowser> browser) {}
+	void OnSetAddress(CefRefPtr<CefBrowser> browser, const wstring& url) {
+		//MessageBoxW(NULL, L"OnSetAddress", L"title", MB_OK);
 	}
-	CoUninitialize();
-	return buf;
-}
-
-void CloseTab(std::wstring id)
-{
-	list<BrowserBundle*>::iterator it;
-	for (it = bundleList.begin(); it != bundleList.end(); it++) 
-	{
-		BrowserBundle* bundle = *it;
-		if (bundle->id == id)
+	void OnSetTitle(CefRefPtr<CefBrowser> browser, const wstring& title) {
+		int nBrowserId = browser->GetIdentifier();
+		for (BrowserBundle* b : bundleList)
 		{
-			static_cast<CContainerUI*>(bundle->tab->GetParent())->Remove(bundle->tab, true);
-			break;
+			b->TabButton->SetText(title.c_str());
 		}
 	}
-}
+	void OnSetFullscreen(CefRefPtr<CefBrowser> browser, bool fullscreen) {
+		//MessageBoxW(NULL, L"OnSetFullscreen", L"title", MB_OK);
+	}
+	void OnSetLoadingState(CefRefPtr<CefBrowser> browser, bool isLoading, bool canGoBack, bool canGoForward) {
+		//MessageBoxW(NULL, L"OnSetLoadingState", L"title", MB_OK);
+	}
+	void OnSetDraggableRegions(CefRefPtr<CefBrowser> browser, const vector<CefDraggableRegion>& regions) {
+		//MessageBoxW(NULL, L"OnSetDraggableRegions", L"title", MB_OK);
+	}
+	void OnNewPage(const wstring& url) {
+		//MessageBoxW(NULL, L"OnNewPage", L"title", MB_OK);
+	}
+	void OnUrlBlocked(const wstring& url) {
+		//MessageBoxW(NULL, L"OnUrlBlocked", L"title", MB_OK);
+	}
+	void OnBeforeDownload(CefRefPtr<CefBrowser> browser,
+		CefRefPtr<CefDownloadItem> download_item,
+		const CefString& suggested_name,
+		CefRefPtr<CefBeforeDownloadCallback> callback) {
+		//MessageBoxW(NULL, L"OnBeforeDownload", L"title", MB_OK);
+	};
+	void OnDownloadUpdated(CefRefPtr<CefBrowser> browser,
+		CefRefPtr<CefDownloadItem> download_item,
+		CefRefPtr<CefDownloadItemCallback> callback) {
+		//MessageBoxW(NULL, L"OnDownloadUpdated", L"title", MB_OK);
+	};
+	void OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefProcessId source_process, CefRefPtr<CefProcessMessage> message) {
+		//MessageBoxW(NULL, L"OnProcessMessageReceived", L"title", MB_OK);
+	}
+	void OnPopupCreated(CefRefPtr<CefBrowser> browser) {
+		//MessageBoxW(NULL, L"OnPopupCreated", L"title", MB_OK);
+	};
 
-virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	//LRESULT lRes = 0;
+	std::wstring GetUniqueid()
+	{
+		wchar_t buf[128] = { 0 };
+		GUID guid;
+		CoInitialize(NULL);
+		if (S_OK == ::CoCreateGuid(&guid))
+		{
+			swprintf(buf, sizeof(buf),
+				L"%08X%04X%04X%02X%02X%02X%02X%02X%02X%02X%02X",
+				guid.Data1,
+				guid.Data2,
+				guid.Data3,
+				guid.Data4[0], guid.Data4[1],
+				guid.Data4[2], guid.Data4[3],
+				guid.Data4[4], guid.Data4[5],
+				guid.Data4[6], guid.Data4[7]);
+		}
+		CoUninitialize();
+		return buf;
+	}
 
-	//if (uMsg == WM_CREATE)
-	//{
-	//	m_PaintManager.Init(m_hWnd);
-	//	m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
-	//	return lRes;
-	//}
+	void CloseTab(std::wstring id)
+	{
+		list<BrowserBundle*>::iterator it;
+		for (it = bundleList.begin(); it != bundleList.end(); it++)
+		{
+			BrowserBundle* bundle = *it;
+			if (bundle->id == id)
+			{
+				static_cast<CContainerUI*>(bundle->tab->GetParent())->Remove(bundle->tab, true);
+				m_CEFBrowser->DelPage(bundle->browserId);
+				break;
+			}
+		}
+	}
 
-	//if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
-	//{
-	//	return lRes;
-	//}
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		//LRESULT lRes = 0;
 
-	return __super::HandleMessage(uMsg, wParam, lParam);
+		//if (uMsg == WM_CREATE)
+		//{
+		//	m_PaintManager.Init(m_hWnd);
+		//	m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
+		//	return lRes;
+		//}
+
+		//if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		//{
+		//	return lRes;
+		//}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
 
 	}
 
-struct BrowserBundle
-{
-	std::wstring id;
-	CContainerUI* tab;
-	BrowserBundle(std::wstring id) : id(id) {}
-};
+	struct BrowserBundle
+	{
+		std::wstring id;
+		CContainerUI* tab;
+		COptionUI* TabButton;
+		int browserId;
+		BrowserBundle(std::wstring id) : id(id) {}
+	};
 
-std::list <BrowserBundle*> bundleList;
+	std::list <BrowserBundle*> bundleList;
 
 private:
 	CHorizontalLayoutUI* m_CurrentTabStrip;
-		
+	XBrowser::CCEFBrowserUI* m_CEFBrowser;
+
 };
+
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	CPaintManagerUI::SetInstance(hInstance);
 	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
+	/**/
+	// 以下为CEF初始化部分
+	// 高清DPI支持
+	//CefEnableHighDPISupport();
+	void* sandbox_info = NULL;
+	// Provide CEF with command-line arguments.
+	CefMainArgs main_args(hInstance);
+	// Specify CEF global settings here.
+	CefSettings settings;
+	CefSettingsTraits::init(&settings);
+	settings.no_sandbox = true;
+
+	CefRefPtr<CefApp> app;
+	XBrowser::InitCefApp(app);
+	// CEF applications have multiple sub-processes (render, plugin, GPU, etc)
+	// that share the same executable. This function checks the command-line and,
+	// if this is a sub-process, executes the appropriate logic.
+	int exit_code = CefExecuteProcess(main_args, app, sandbox_info);
+	if (exit_code >= 0)
+	{
+		// The sub-process has completed so return here.
+		return exit_code;
+	}
+
+	CefInitialize(main_args, settings, app.get(), sandbox_info);
 
 	CDuiFrameWnd duiFrame;
 	duiFrame.Create(NULL, _T("DuilibDemo"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
 	duiFrame.CenterWindow();
-	duiFrame.ShowModal();
+	duiFrame.ShowWindow();
+
+	// 开启CEF的消息循环
+	CefRunMessageLoop();
+	// DuiLib结束、清理
+	DuiLib::CPaintManagerUI::Term();
+	// CEF结束、清理
+	CefShutdown();
+
 	return 0;
 }
